b3da7eb4c8ae7903df1fd0a7e0a093649db3c859
3xscola <4cruxt.unknown@gmail.com>
2022-07-19 19:45:00 -0700

    Created health portion

--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -1,13 +1,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public abstract class Item : ScriptableObject
+[CreateAssetMenu(fileName = "New Item", menuName = "Item", order = 0)]
+public class Item : ScriptableObject
 {
     public string itemName;
     public int price;
     [TextArea]
     public string description;
     public Sprite image;
-
-    public abstract void Use();
 }
