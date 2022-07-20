6d341666e8fb87c634cf91aa733e7f2b86937b69
3xscola <4cruxt.unknown@gmail.com>
2022-07-19 19:36:23 -0700

    Created item scriptable object

--- Assets/Resources/Item.cs
+++ Assets/Resources/Item.cs
@@ -1,18 +1,11 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Item : MonoBehaviour
+public class Item : ScriptableObject
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public string itemName;
+    public int price;
+    [TextArea]
+    public string description;
+    public Sprite image;
 }
